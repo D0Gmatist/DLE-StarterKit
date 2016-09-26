7f323fab80b633d4e5157efa687f896eb226253c
D0Gmatist <375256232834@yandex.by>
2016-09-26 19:09:23 +0300

    Update InitialClass.php

--- StarterKit/Classes/InitialClass.php
+++ StarterKit/Classes/InitialClass.php
@@ -2,7 +2,32 @@
 
 namespace StarterKit\Classes;
 
+	/**
+	 * @throws Exception
+	 */
+	public function checkBeforeInstall() {
+		$this->logs[] = 'checkBeforeInstall';
+
+		if ( isset( $this->cfg['minVersion'] ) ) {
+			if ( $this->dle_config['version_id'] < $this->cfg['minVersion'] ) {
+				$this->error_text = 'Установленная версия DLE слишком старая. Необходимо установить DLE не ниже ' . $this->cfg['minVersion'];
+
+			}
+
+			if ( $this->dle_config['version_id'] > $this->cfg['maxVersion'] ) {
+				$this->error_text = 'Установленная версия DLE слишком новая. Необходимо установить DLE не выше ' . $this->cfg['maxVersion'];
+
+			}
+
+		} else {
+			$this->error_text = 'Файл с конфигурацией установки не найден, возмжно установочные файлы модуля не скопированы.';
+
+		}
+
+	}
+
 class InitialClass extends StarterKitClass {
+		$this->logs[] = 'StarterKitClass';
 
 	public $step					= 1;
 	public $next_btn				= FALSE;
